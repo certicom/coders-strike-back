a1bf6b04cc1014cb8d4fa6102bef89e0e3421df5
Alexandre <alexandre.lepoittevin@efrei.net>
2019-01-24 19:11:23 +0100

    Good idea but inefficient :(

--- main.cpp
+++ main.cpp
@@ -2,8 +2,69 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
+
+class Point
+{
+public:
+
+	Point() : x(0), y(0) {}
+
+	Point(int _x, int _y) : x(_x), y(_y) {}
+
+	//Point(const Point& other) {x = other.x; y = other.y;}
+
+	bool operator==(const Point& other)const { return (x == other.x && y == other.y); }
+
+	bool operator!=(const Point& other)const { return (x != other.x || y != other.y); }
+
+	// a vector operation but same thing
+	int Norme() { return sqrtf(x*x + y*y); }
+
+	int x;
+	int y;
+};
+
+
+bool KnowTheField(vector<Point>& list)
+{
+	if (list.size() == 0)
+		return false;
+
+	Point p = list[0];
+
+	for (int i = 1; i<list.size(); i++)
+	{
+		if (list[i] == p)
+			return true;
+	}
+}
+
+void LearnTheField(int x, int y, vector<Point>& list)
+{
+	if (KnowTheField(list))
+		return; // easier
+
+	if (list.size() == 0 || Point(x, y) != list[list.size() - 1])
+	{
+		list.push_back(Point(x, y));
+	}
+}
+
+Point GetNextCheckpoint(int x, int y, vector<Point>& list)
+{
+	Point p(x, y);
+
+	for (int i = 0; i<list.size(); i++)
+	{
+		if (list[i] == p)
+			return i == list.size() - 1 ? list[0] : list[i + 1];
+	}
+
+	return p;
+}
 
 /**
 * Auto-generated code below aims at helping you parse
@@ -25,26 +86,74 @@ int main()
 		int opponentY;
 		cin >> opponentX >> opponentY; cin.ignore();
 
-		// Write an action using cout. DON'T FORGET THE "<< endl"
-		// To debug: cerr << "Debug messages..." << endl;
+		//----------------------------------------------------------------------
+		//----------------------------------------------------------------------
 
+		// This part handle the learning of the checkpoints
+		static std::vector<Point> checkpointList;
 
-		// You have to output the target position
-		// followed by the power (0 <= thrust <= 100)
-		// i.e.: "x y thrust"
+		static bool isKnowingTheField = false;
+
+		LearnTheField(nextCheckpointX, nextCheckpointY, checkpointList);
+
+		if (!isKnowingTheField && KnowTheField(checkpointList))
+		{
+			isKnowingTheField = true;
+			checkpointList.resize(checkpointList.size() - 1);
+			// now we have a clean representation of all the checkpoints in order
+		}
+
+		//----------------------------------------------------------------------
+		//----------------------------------------------------------------------
+
+		static Point lastPos(x, y);
+
+		Point speed(x - lastPos.x, y - lastPos.y); // more a vector, but same structure
+
 		string power;
 
 		int angle = abs(nextCheckpointAngle);
 
-		if (angle < 90)
-			power = "100";
+		cerr << speed.Norme() << endl;
+
+		Point target;
+
+		if (isKnowingTheField)
+		{
+			// all the point of learning the checkpoint is for this adrifting effect
+			if (speed.Norme()*1.5 > nextCheckpointDist)
+			{
+				target = GetNextCheckpoint(x, y, checkpointList); // well, the one after the normal logic target
+				power = "0";
+			}
+			else
+			{
+				target = Point(nextCheckpointX, nextCheckpointY);
+
+				if (angle > 90)
+					power = "0";
+				else
+					power = "100";
+			}
+		}
 		else
-			power = "0";
+		{
+			target = Point(nextCheckpointX, nextCheckpointY);
+
+			if (angle > 90)
+				power = "0";
+			else
+				power = "100";
+		}
 
 
-		if (nextCheckpointDist > 8000 && angle < 5)
-			power = "BOOST";
 
-		cout << nextCheckpointX << " " << nextCheckpointY << " " << power << endl;
+		/*
+		if(nextCheckpointDist > 4000 && angle < 5 )
+		power = "BOOST";
+		*/
+		lastPos = Point(x, y);
+
+		cout << target.x << " " << target.y << " " << power << endl;
 	}
 }
